94d2a45e680eee682dd0c7ce9a6d2b35b133b6b1
Jayhun Rahim <jayhun9999@gmail.com>
2025-12-01 17:18:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,12 @@ window.addEventListener('load', () => {
   });
 });
 
+//Header’a Scroll Shadow Eklemek Header’a Scroll Shadow Eklemek Header’a Scroll Shadow Eklemek Header’a Scroll Shadow Eklemek Header’a Scroll Shadow Eklemek//
+window.addEventListener("scroll", function () {
+  const header = document.querySelector("header");
+  if (window.scrollY > 10) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
