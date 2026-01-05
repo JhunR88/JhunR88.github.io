7e7a5d10c4417b88262dc6353c1675e0dce2cf1c
Jayhun Rahim <jayhun9999@gmail.com>
2026-01-05 23:34:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -27,3 +27,29 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+// Project Showcase Geliştirme Project Showcase Geliştirme Project Showcase Geliştirme Project Showcase Geliştirme Project Showcase Geliştirme
+const filterButtons = document.querySelectorAll(".filter-btn");
+const projectCards = document.querySelectorAll(".project-card");
+
+filterButtons.forEach(button => {
+  button.addEventListener("click", () => {
+
+    // Active class temizle
+    filterButtons.forEach(btn => btn.classList.remove("active"));
+    button.classList.add("active");
+
+    const filter = button.getAttribute("data-filter");
+
+    projectCards.forEach(card => {
+      const category = card.getAttribute("data-category");
+
+      if (filter === "all" || category === filter) {
+        card.style.display = "block";
+      } else {
+        card.style.display = "none";
+      }
+    });
+
+  });
+});
